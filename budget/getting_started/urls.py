3b9722c787f05c189428d6480cad91de4adc04e4
Scott Reese <scott_reese@pm.me>
2019-10-07 11:39:32 -0400

    fixed flake8 errors

--- budget/getting_started/urls.py
+++ budget/getting_started/urls.py
@@ -4,7 +4,19 @@ from django.urls import path
 from .views import *
 
 url_patterns = [
-    path('gettingstarted/income', GettingStarted.as_view(), name='getting_started'),
-    path('gettingstarted/bills', GettingStarted.as_view(), name='getting_started'),
-    path('gettingstarted/accounts', GettingStarted.as_view(), name='getting_started')
+    path(
+        'gettingstarted/income',
+        GettingStarted.as_view(),
+        name='getting_started'
+        ),
+    path(
+        'gettingstarted/bills',
+        GettingStarted.as_view(),
+        name='getting_started_bills'
+        ),
+    path(
+        'gettingstarted/accounts',
+        GettingStarted.as_view(),
+        name='getting_started_accounts'
+        ),
 ]
