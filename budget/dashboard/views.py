93752f2f15391fa69379220fc92ed44b43c22d67
Scott Reese <scott_reese@pm.me>
2019-10-08 19:15:38 -0400

    added pie chart generation

--- budget/dashboard/views.py
+++ budget/dashboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
+from budget.bill.models import Bill
+
 
 class Dashboard(TemplateView):
     def get(self, request, *args, **kwargs):
@@ -14,12 +16,15 @@ class Chart(TemplateView):
     def get(self, request, *args, **kwargs):
         import matplotlib
         matplotlib.use('TkAgg')
+        matplotlib.use('Agg')
         import matplotlib.pyplot as plt
+        user = request.user
+        bills = Bill.objects.filter(owner=user.client)
+        total = sum([bill.amount for bill in bills])
 
-        labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-        sizes = [15, 30, 45, 10]
-        explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
+        sizes = [bill.amount/total for bill in bills]
+        labels = [bill.title for bill in bills]
+        explode = [0 if size >= 0.05 else 0.5 for size in sizes]
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
                 shadow=True, startangle=90)
