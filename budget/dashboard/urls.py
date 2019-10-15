bb43ec55ea2ddf3befa598826ea9fcaaaa475ed0
Scott Reese <scott_reese@pm.me>
2019-10-15 18:57:49 -0400

    minor layout changes related to money_bar and charts

--- budget/dashboard/urls.py
+++ budget/dashboard/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from .views import *
 
 url_patterns = [
-    path('dashboard', login_required(Dashboard.as_view()), name='dashboard'),
+    path('dashboard/', login_required(Dashboard.as_view()), name='dashboard'),
     path('', login_required(Dashboard.as_view()), name='index'),
-    path('dashboard/<id>', login_required(Dashboard.as_view()), name='dashboard_id')
+    path('dashboard/<id>/', login_required(Dashboard.as_view()), name='dashboard_id')
 ]
