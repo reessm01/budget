81f5735466b85c967bc32e9c473ab26a8f9e61a1
Scott Reese <scott_reese@pm.me>
2020-05-03 20:00:18 -0400

    Feature dynamic charting (#15)
    * enabled debugging for dev
    
    * removed matplotlib & numpy for canvasjs integration
    
    * pipenv correction

--- budget/dashboard/views.py
+++ budget/dashboard/views.py
@@ -13,114 +13,6 @@ from budget.check_in.forms import CheckInForm
 
 
 class Dashboard(TemplateView):
-    def save_chart(self, user, total, sizes, labels, path):
-        import matplotlib
-        matplotlib.use('TkAgg')
-        matplotlib.use('Agg')
-        import matplotlib.pyplot as plt
-        
-        explode = [0 if size >= 0.05 else 0.25 for size in sizes]
-        fig1, ax1 = plt.subplots()
-        wedges, _ = ax1.pie(sizes, explode=explode, shadow=True)
-        # Equal aspect ratio ensures that pie is drawn as a circle.
-        ax1.axis('equal')
-        labels = [
-            '{0} - {1:1.1f} %'.format(label, size*100)
-            for label, size in zip(labels, sizes)
-        ]
-        ax1.legend(
-            wedges,
-            labels,
-            loc='center left',
-            bbox_to_anchor=(1, 0, 0.5, 1),
-            prop=dict(size=16)
-        )
-        ax1.axes.get_xaxis().set_visible(False)
-        ax1.axes.get_yaxis().set_visible(False)
-
-        plt.subplots_adjust(
-            top=1,
-            bottom=0,
-            right=1,
-            left=0,
-            hspace=0,
-            wspace=0
-        )
-        plt.margins(0, 0)
-        plt.rcParams['legend.title_fontsize'] = 'large'
-
-        plt.savefig(
-            path,
-            bbox_inches='tight',
-            dpi=100,
-            quality=100,
-            pad_inches=0.015
-        )
-
-    def process_previous_filename(self, user, title, image_directory, path, remove):
-        previous_filename = ''
-        for image in image_directory:
-            if f'{user.id}_{title}' in image:
-                previous_filename = image
-                break
-
-        if previous_filename and remove:
-            os.remove(path + previous_filename)
-
-        return previous_filename
-
-    def create_charts(self, user):
-        path = './budget/static/img/'
-        image_directory = os.listdir(path)
-        chart_info = [
-            ('bills', Bill.objects.filter(owner=user.client)),
-            ('income', Income.objects.filter(owner=user.client))
-        ]
-
-        images = []
-
-        for chart in chart_info:
-            title, query = chart[0], chart[1].order_by('-amount')
-            last_modified = [
-                entry.last_modified
-                for entry in query.order_by('-last_modified')
-            ][0]
-            filename = f'{user.id}_{title}_{last_modified}.png'
-
-            if filename not in image_directory:
-                previous_filename = self.process_previous_filename(
-                    user, title, image_directory, path, True)
-                if not previous_filename:
-                    if title == 'bills':
-                        total = sum([entry.amount for entry in query])
-                        sizes = [entry.amount/total for entry in query]
-                        labels = [entry.title for entry in query]
-                        self.save_chart(user, total, sizes,
-                                        labels, path + filename)
-                    else:
-                        income_total = sum(
-                            [entry.amount*entry.frequency.number_of_paychecks for entry in query])
-                        bills_total = sum([
-                            entry.amount*entry.frequency.number_of_paychecks for entry in chart_info[0][1]
-                        ])
-                        total = income_total + bills_total
-                        sizes = [
-                            entry/total
-                            for entry in [income_total, bills_total]
-                        ]
-                        labels = [entry for entry in ['income', 'bills']]
-                        self.save_chart(user, total, sizes,
-                                        labels, path + filename)
-                    images.append('img/' + filename)
-                else:
-                    images.append('img/' + previous_filename)
-
-            else:
-                previous_filename = self.process_previous_filename(
-                    user, title, image_directory, path, False)
-                images.append('img/' + previous_filename)
-
-        return images
 
     def get_checkin_info(self, client, entry_id):
         check_ins = CheckIn.objects.filter(user=client).order_by('date')
@@ -149,6 +41,24 @@ class Dashboard(TemplateView):
             else:
                 previous_check_in = None
         return (selected, next_check_in, previous_check_in)
+
+    def chart_serializer(self, datas):
+        total = sum([data.amount for data in datas])
+
+        return [{'y': float(data.amount/total*100), 'label': data.title} for data in datas]
+
+    def get_debt_ratio(self, bills, incomes):
+        total_income = float(sum([income.amount * income.frequency.number_of_paychecks for income in incomes]))
+        total_debts = float(sum([bill.amount * bill.frequency.number_of_paychecks for bill in bills]))
+        total_surplus = (total_income - total_debts)/total_income * 100
+        total_debts = total_debts / total_income * 100
+        return [{
+            'y': total_debts,
+            'label': 'bills'
+        },
+        {
+            'y': total_surplus,
+            'label': 'income'}]
 
     def get(self, request, *args, **kwargs):
         page = 'dashboard.html'
@@ -180,8 +90,6 @@ class Dashboard(TemplateView):
                 entry_id = None
 
             try:
-                file_paths = self.create_charts(user)
-                bills, incomes = file_paths[0], file_paths[1]
                 check_in, next_id, prev_id = self.get_checkin_info(
                     user.client, entry_id)
                 form = CheckInForm(checkin_id=check_in.id)
@@ -205,9 +113,15 @@ class Dashboard(TemplateView):
                 else:
                     prev_link = f'{base_link}{prev_id}'
 
+                bills_2 = Bill.objects.filter(owner=user.client)
+                income = Income.objects.filter(owner=user.client)
+                
+                bills_data = self.chart_serializer(bills_2)
+                debt_ratio_data = self.get_debt_ratio(bills_2, income)
+                debt_ratio_chart = {'title': 'debt_ratios', 'data': bills_data}
+                income_debt_ratio_chart = {'title': 'income_to_debt_ratios', 'data': debt_ratio_data}
+                print(debt_ratio_chart)
                 return render(request, page, {
-                    'bills': bills,
-                    'incomes': incomes,
                     'outbound': outbound,
                     'remaining_p': remaining,
                     'check_in': check_in,
@@ -217,9 +131,11 @@ class Dashboard(TemplateView):
                     'back': prev_link,
                     'form': form,
                     'subtitle_text_style': subtitle_text_style,
-                    'today': datetime.date.today()
+                    'today': datetime.date.today(),
+                    'debt_ratio_chart': debt_ratio_chart,
+                    'income_debt_ratio_chart': income_debt_ratio_chart
                 })
-            except Exception:
+            except Exception as e:
                 return HttpResponseServerError()
         else:
             return HttpResponseRedirect(reverse('getting_started'))
